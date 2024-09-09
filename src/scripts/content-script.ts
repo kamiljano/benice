2e48238a539909e8c282f9543a29f65bd63e17e6
Janowski, Kamil 1 (External) <kamil.1.janowski_external@wartsila.com>
2024-09-09 18:23:52 +0300

    adding the icon in the pages

--- src/scripts/content-script.ts
+++ src/scripts/content-script.ts
@@ -1,18 +1,44 @@
 import debounce from 'debounce';
 
-console.debug('Be nice extension active');
-
-const textareas = document.querySelectorAll('textarea');
+const ignorePages = new Set<string>(
+  ['google.com', 'translate.google.com']
+    .reduce<string[][]>((acc, domain) => {
+      acc.push([domain, `www.${domain}`]);
+      return acc;
+    }, [])
+    .flat(),
+);
 
 const publishTextChange = (ev: Event) => {
-  console.log('aaaaaaaaaa', ev);
-  chrome.runtime.sendMessage({ type: 'text-change', text: (ev.target as HTMLTextAreaElement).value });
+  chrome.runtime.sendMessage({
+    type: 'text-change',
+    text: (ev.target as HTMLTextAreaElement).value,
+  });
 };
 
-textareas.forEach((textarea) => {
-  textarea.onkeyup = debounce(publishTextChange, 500);
-  // const rect = textarea.getBoundingClientRect();
-  // console.log(
-  //   `Textarea ${index + 1}: Top=${rect.top}, Left=${rect.left}, Bottom=${rect.bottom}, Right=${rect.right}`,
-  // );
-});
+const addIcon = (textarea: HTMLTextAreaElement) => {
+  const rect = textarea.getBoundingClientRect();
+
+  const icon = document.createElement('img');
+  icon.src = chrome.runtime.getURL('images/logo-48.png');
+  icon.style.position = 'fixed';
+  icon.style.top = `${rect.top + 5}px`;
+  icon.style.left = `${rect.left + rect.width + 5}px`;
+  icon.style.width = '20px';
+  icon.style.height = '20px';
+  icon.style.cursor = 'pointer';
+
+  textarea.parentNode?.insertBefore(icon, textarea);
+};
+
+if (!ignorePages.has(window.location.hostname)) {
+  console.debug('[BeNice]: extension active', window.location.hostname);
+  const textareas = document.querySelectorAll('textarea');
+
+  textareas.forEach((textarea) => {
+    textarea.onkeyup = debounce(publishTextChange, 500);
+    addIcon(textarea);
+  });
+} else {
+  console.debug('[BeNice]: Ignoring page:', window.location.hostname);
+}
